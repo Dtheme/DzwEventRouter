cdfb11bc6e7acd5bbedc85c39b8ab31b828b18d8
aaronduan@zego.im <aaronduan@zego.im>
2024-10-29 10:17:14 +0800

    udpate

--- DzwEventRouter/DzwEventRouter/DzwEventRouter/EventRouterHeader.h
+++ DzwEventRouter/DzwEventRouter/DzwEventRouter/EventRouterHeader.h
@@ -3,7 +3,7 @@
 //  DzwEventRouter
 //
 //  Created by dzw on 2024/10/23.
-//  Copyright © 2024 juangua. All rights reserved.
+//  Copyright © 2024 dzw. All rights reserved.
 //
 
 #ifndef EventRouterHeader_h
